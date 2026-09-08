8f128c76ba840876b4f683c5f3c55411d2525427
Abhay Pratap Singh Chandel <2k24.psitcsdsa2411165@gmail.com>
2026-09-09 00:18:34 +0530

    Completed Mini Project for Baic UI-design (Footer Added)

--- UI_Project/src/App.jsx
+++ UI_Project/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TopSection from './Components/TopSection/TopSection'
 import MiddleSection from './Components/MiddleSection/MiddleSection'
+import Footer from './Components/BottomSection/Footer'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <TopSection />
       <MiddleSection />
+      <Footer />
     </div>
   )
 }
--- UI_Project/src/Components/BottomSection/Footer.jsx
+++ UI_Project/src/Components/BottomSection/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='w-full bg-gray-900 text-gray-200 py-4 px-6 text-center'>
+      <p>DevHell UI. All rights reserved.</p>
     </div>
   )
 }
