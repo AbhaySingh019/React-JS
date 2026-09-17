4670733e489d7e9f4a87d757d755ddb23ceb3b2f
Abhay Pratap Singh Chandel <2k24.psitcsdsa2411165@gmail.com>
2026-09-17 12:41:45 +0530

    Author Name added and images are connected to external link (will get open in new tab every time they get clicked)

--- Gallery_Project/src/App.jsx
+++ Gallery_Project/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const[userData,setUserData] = useState([ ]);
 
   const getData = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=20')
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
 
     setUserData(response.data)
     console.log(response.data)
@@ -17,24 +17,29 @@ const App = () => {
   if(userData.length>0){
     printUserData = userData.map(function(elem,idx){
 
-      return <div className='h-35 w-37'>
+      return <a href={elem.url} target='_blank'>
+        <div>
+        <div className='h-35 w-37 border-2 '>
         <img className='h-full object-cover'
-        src={elem.download_url} alt="" />
+        src={elem.download_url} alt="Image" />
       </div>
-
+      <h2>{elem.author}</h2>
+      </div>
+      </a>
     })
   }
 
   return (
-    <div className='bg-black h-screen p-4 text-white'>
+    <div className='bg-black h-screen p-4 text-white overflow-auto'>
       <button 
       onClick={getData}
       className='bg-amber-50 text-red-600 m-4 px-2 active:scale-95 border-green-300 rounded-3xl border-4 '>
       GET DATA</button>
 
-      <div className='flex flex-wrap gap-3'>
+      <div className='flex flex-wrap gap-3 '>
         {printUserData}
       </div>
+      
     </div>
   )
 }
